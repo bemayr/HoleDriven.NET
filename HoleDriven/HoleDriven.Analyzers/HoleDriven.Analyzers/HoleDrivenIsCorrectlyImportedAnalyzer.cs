e8b1c769243a9a00a5d53b4b23c123068e381539
bemayr <bernhard@mayr.io>
2023-08-08 11:13:49 +0200

    Add Analyzer Stub

---
+++ HoleDriven/HoleDriven.Analyzers/HoleDriven.Analyzers/HoleDrivenIsCorrectlyImportedAnalyzer.cs
@@ -0,0 +1,8 @@
+﻿namespace HoleDriven.Analyzers
+{
+    public class HoleDrivenIsCorrectlyImportedAnalyzer
+    {
+        // TODO: make sure that HoleDriven is imported like using HoleDriven and no static imports, so that the analyzers can actually detect Hole.<...>
+        // this is a todo comment because I didn't yet look into how to use analyzers in their own project
+    }
+}
